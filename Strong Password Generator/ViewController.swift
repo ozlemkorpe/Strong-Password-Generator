3726f2ea72f9c534bc6f21ed1aff1d4939885293
Özlem Körpe <cse.ozlemkorpe@gmail.com>
2023-05-24 01:08:51 +0300

    Switch actions are implemented

--- Strong Password Generator/ViewController.swift
+++ Strong Password Generator/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var possiblePasswordCharactersSet = Set<String>()
+    var passwordLength = 6
+    
+    // Character sets
+    let lowerCaseAlphabetSet: Set<String> = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"]
+    let upperCaseAlphabetSet: Set<String> = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    let numbersSet: Set<String> = ["0","1","2","3","4","5","6","7","8","9"]
+    let specialCharactersSet: Set<String> = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", "|", "\\", ":", ";", "'", "\"", "<", ">", ",", ".", "?", "/", "~"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        possiblePasswordCharactersSet = possiblePasswordCharactersSet.union(lowerCaseAlphabetSet)
+        //print("Possible characters are: \(possiblePasswordCharactersSet)")
     }
 
-
+    @IBAction func includeNumbersEnabled(_ sender: UISwitch) {
+        
+        if(sender.isOn){
+            print("Including numbers is true")
+            possiblePasswordCharactersSet = possiblePasswordCharactersSet.union(numbersSet)
+        }else{
+            print("Including numbers is false")
+            possiblePasswordCharactersSet = possiblePasswordCharactersSet.subtracting(numbersSet)
+        }
+        print("Possible characters are: \(possiblePasswordCharactersSet)")
+    }
+    
+    @IBAction func includeUpperCaseEnabled(_ sender: UISwitch) {
+        if(sender.isOn){
+            print("Including upper case letters is true")
+            possiblePasswordCharactersSet = possiblePasswordCharactersSet.union(upperCaseAlphabetSet)
+        }else{
+            print("Including upper case letters is false")
+            possiblePasswordCharactersSet = possiblePasswordCharactersSet.subtracting(upperCaseAlphabetSet)
+        }
+        print("Possible characters are: \(possiblePasswordCharactersSet)")
+    }
+    
+    @IBAction func includeSpecialCharactersEnabled(_ sender: UISwitch) {
+        if(sender.isOn){
+            possiblePasswordCharactersSet = possiblePasswordCharactersSet.union(specialCharactersSet)
+        }else{
+            possiblePasswordCharactersSet = possiblePasswordCharactersSet.subtracting(specialCharactersSet)
+        }
+        print("Possible characters are: \(possiblePasswordCharactersSet)")
+    }
 }
 
